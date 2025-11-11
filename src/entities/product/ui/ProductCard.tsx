a04e983f9e8db825cd1d8aa640344332843c8fe8
Vitalii <v.95vitalii@gmail.com>
2025-11-11 12:49:48 -0600

    feat(productCard): -add function for "add to bag" btn

--- src/entities/product/ui/ProductCard.tsx
+++ src/entities/product/ui/ProductCard.tsx
@@ -1,12 +1,16 @@
 import "./productCard.css";
 import shopingCart from "@/shared/assets/shopping-cart.png";
 
+import {useCart} from "@/features/cart/useCart";
+
 import type {Product} from "@/features/products/api";
 
 type Props = {
   product: Product;
 };
 export const ProductCard = ({product}: Props) => {
+  const {addProduct} = useCart();
+
   return (
     <div className='card__container'>
       <div className='card__image'>
@@ -17,7 +21,10 @@ export const ProductCard = ({product}: Props) => {
         <div className='card__description'>{product.des}</div>
         <div className='card__bottom-line'>
           <div className='card__price'>{`$${product.price}`}</div>
-          <div className='card__add-to-bag-icon'>
+          <div
+            className='card__add-to-bag-icon'
+            onClick={() => addProduct(product)}
+          >
             <img src={shopingCart} alt='Add' />
           </div>
         </div>
