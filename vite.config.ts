1b110bb9989888fc30c668d435597e20540e85fe
Vitalii <v.95vitalii@gmail.com>
2025-11-12 21:13:27 -0600

    fix(deploy): -add settings for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/e-commerce/",
   plugins: [react()],
   resolve: {
     alias: {"@": path.resolve(__dirname, "src")}
